9f6451fa0cb807eff30f7c43162b78f16fe0c3fd
Jason <el.jsteer@gmail.com>
2022-09-23 18:06:42 +1000

    added in remaining JS code files and requests and also src-sw.js for implementing service workers

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,31 +15,33 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
+
+    // Create a connection to the database database and version we want to use.
+  const connectDb = await openDB('jate', 1);
+
+   // Create a new transaction and specify the database and data privileges.
+  const tx = connectDb.transaction('jate', 'readwrite');
+
+    // Open up the desired object store.
   const store = tx.objectStore('jate');
+
+    // Use the .put method to add/update data in the database.
   const request = store.put({ id: 1, value: content });
+
+    // Get confirmation of the request.
   const result = await request;
-  console.log('data saved to the database', result.value);
+    console.log('data saved to the database', result.value);
 };
 // TODO: Add logic for a method that gets all the content from the database
 // export const getDb = async () => console.error('getDb not implemented');
 export const getDb = async () => {
   console.log('GET from the database');
-
-  // Create a connection to the database database and version we want to use.
-  const jateDb = await openDB('jate', 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readonly');
-
-  // Open up the desired object store.
+  const connectDb = await openDB('jate', 1);
+  const tx = connectDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-
-  // Use the .getAll() method to get all data in the database.
+  // Use the .getAll method to get all data in the database.
   const request = store.get(1);
 
-  // Get confirmation of the request.
   const result = await request;
 
   result ? console.log('data received from the database', result.value) :
@@ -47,6 +49,7 @@ export const getDb = async () => {
 
   return result.value;
 };
-initdb();
 
 initdb();
+
+
